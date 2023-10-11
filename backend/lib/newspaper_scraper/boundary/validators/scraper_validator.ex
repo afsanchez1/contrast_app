e36e608d5b1968af48ca1c5ecd27383608962733
afsanchez1 <a.fsanchez1@udc.es>
2023-10-11 14:01:40 +0200

    ScraperValidator tests and updates implemented

--- backend/lib/newspaper_scraper/boundary/validators/scraper_validator.ex
+++ backend/lib/newspaper_scraper/boundary/validators/scraper_validator.ex
@@ -6,6 +6,9 @@ defmodule NewspaperScraper.Boundary.ScraperValidator do
     "elpais.com"
   ]
 
+  def max_limit, do: @max_limit
+  def base_urls, do: @base_urls
+
   def search_articles_errors(fields) when is_map(fields) do
     []
     |> mandatory(fields, :topic, &validate_topic/1)
@@ -21,7 +24,7 @@ defmodule NewspaperScraper.Boundary.ScraperValidator do
   end
 
   defp validate_topic(topic) when is_binary(topic) do
-    check(not String.match?(topic, ~r{^(?=\s*$)}), {:error, "is mandatory"})
+    check(validate_strings(topic), {:error, "is mandatory"})
   end
 
   defp validate_topic(_topic), do: {:error, "must be a binary"}
@@ -34,9 +37,22 @@ defmodule NewspaperScraper.Boundary.ScraperValidator do
     check(limit <= @max_limit, {:error, "max. limit exceeded"})
   end
 
-  defp validate_limit(_limit), do: {:error, "must be a binary"}
+  defp validate_limit(_limit), do: {:error, "must be an integer"}
 
   defp validate_url(url) when is_binary(url) do
+    with :ok <- check(validate_strings(url), {:error, "is mandatory"}),
+         :ok <- check(validate_newspaper_url(url), {:error, "must be a valid newspaper"}),
+         do: :ok,
+         else: (error -> error)
+  end
+
+  defp validate_url(_url), do: {:error, "must be a binary"}
+
+  defp validate_strings(str) do
+    not String.match?(str, ~r{^(?=\s*$)})
+  end
+
+  defp validate_newspaper_url(url) do
     Enum.map(
       @base_urls,
       fn base_url ->
@@ -44,8 +60,5 @@ defmodule NewspaperScraper.Boundary.ScraperValidator do
       end
     )
     |> Enum.reduce(fn elem, acc -> acc or elem end)
-    |> check({:error, "must be a valid newspaper"})
   end
-
-  defp validate_url(_url), do: {:error, "must be a binary"}
 end
