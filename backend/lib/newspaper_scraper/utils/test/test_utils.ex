d18da8c1c66beed1f32e28dca76561741a0cafa8
afsanchez1 <a.fsanchez1@udc.es>
2023-11-08 01:00:35 +0100

    test_utils tests implemented

--- backend/lib/newspaper_scraper/utils/test/test_utils.ex
+++ backend/lib/newspaper_scraper/utils/test/test_utils.ex
@@ -1,8 +1,7 @@
 defmodule NewspaperScraper.Utils.Test.TestUtils do
   def read_and_parse_JSON!(file_path, opts \\ []) do
-    with {:ok, body} <- File.read(file_path),
-         {:ok, parsed_json} <- Jason.decode(body, opts),
-         do: parsed_json,
-         else: (e -> raise e)
+    body = File.read!(file_path)
+
+    Jason.decode!(body, opts)
   end
 end
