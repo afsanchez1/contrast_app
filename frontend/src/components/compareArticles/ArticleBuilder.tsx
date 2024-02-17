af1f5406e2711a904bd186b164b60a7e62eb13a1
afsanchez1 <a.fsanchez1@udc.es>
2024-02-17 09:16:07 +0100

    Article information display completed

--- frontend/src/components/compareArticles/ArticleBuilder.tsx
+++ frontend/src/components/compareArticles/ArticleBuilder.tsx
@@ -1,6 +1,8 @@
-import { Box, Heading, Text, VStack } from '@chakra-ui/react'
+import { Box, Flex, HStack, Heading, Link, Text, VStack } from '@chakra-ui/react'
 import { type FC } from 'react'
 import { type Article } from '../../types'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { parseDateTime } from '../../utils'
 /**
  * Props for ComponentBuilder
  */
@@ -17,28 +19,50 @@ export interface ArticleBuilderProps {
 export const ArticleBuilder: FC<ArticleBuilderProps> = ({ article }) => {
     return (
         <VStack textAlign='left' overflowY='auto' maxH='53vh' padding='1rem'>
-            <Heading
-                as='h1'
-                fontSize={{
-                    base: '1.75rem',
-                    md: '2rem',
-                }}
-                mb='1rem'
-            >
-                {article.headline}
-            </Heading>
-            <Heading
-                as='h2'
-                fontSize={{
-                    base: '1.2rem',
-                    md: '1.5rem',
-                }}
-                fontWeight='medium'
-                mb='1rem'
-            >
-                {article.subheadline}
-            </Heading>
-            <Box>
+            <Box as='section'>
+                <Link href={article.url} isExternal={true}>
+                    <HStack spacing='0.5rem'>
+                        <Heading
+                            as='h1'
+                            fontSize={{
+                                base: '1.75rem',
+                                md: '2rem',
+                            }}
+                            mb='1rem'
+                        >
+                            {article.headline}
+                        </Heading>
+                        <ExternalLinkIcon />
+                    </HStack>
+                </Link>
+                <Heading
+                    as='h2'
+                    fontSize={{
+                        base: '1.2rem',
+                        md: '1.5rem',
+                    }}
+                    fontWeight='medium'
+                    mb='1rem'
+                >
+                    {article.subheadline}
+                </Heading>
+            </Box>
+            <Flex as='section' textAlign='left' justifyItems='left' w='100%' direction='column'>
+                {article.authors.map((author, index) => {
+                    return author.url != null ? (
+                        <Link key={index} href={author.url} isExternal={true}>
+                            <HStack spacing='0.5rem'>
+                                <Text key={index}>{author.name}</Text>
+                                <ExternalLinkIcon />
+                            </HStack>
+                        </Link>
+                    ) : (
+                        <Text key={index}>{author.name}</Text>
+                    )
+                })}
+                <Text>{article.newspaper + ' - ' + parseDateTime(article.last_date_time)}</Text>
+            </Flex>
+            <Box as='section' mt='1rem'>
                 {article.body.map((item, index) => {
                     const tag = Object.keys(item)[0]
                     const content = item[tag]
